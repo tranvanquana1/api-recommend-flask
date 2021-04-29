c98bcf2a38ec9169a6e0db85d4d93a49b20e301e
tvQuan <tranvanquana1@gmail.com>
2021-04-29 09:31:28 +0700

    update api register

--- resources/Routes.py
+++ resources/Routes.py
@@ -1,5 +1,5 @@
 from .Movie import MoviesResource, MovieResource
-from .User import UserResource, LoginResource, UsersResource
+from .User import UserResource, UsersResource, LoginResource, RegisterResource
 from .Predict import PredictResource
 
 
@@ -10,3 +10,4 @@ def initialize_routes(api):
     api.add_resource(UserResource, '/api/user')
     api.add_resource(UsersResource, '/api/list-user')
     api.add_resource(LoginResource, '/api/login')
+    api.add_resource(RegisterResource, '/api/register')
--- resources/User.py
+++ resources/User.py
@@ -39,47 +39,6 @@ class UserResource(Resource):
     def post(self):
 
         args = parser.parse_args()
-        username = args['username']
-        password = args['password']
-
-        if not username:
-            return jsonify({
-                'message': 'username bat buoc nhap'
-            })
-
-        if not password:
-            return jsonify({
-                'message': 'password bat buoc nhap'
-            })
-
-        user = Users.objects(username=username).first()
-
-        if not user:
-            try:
-                user_id = Users.objects().count() + 1
-                new_user = Users(user_id=user_id, username=username,
-                                 password=password)
-                new_user.save()
-
-                return jsonify({
-                    "message": 'Tao tai khoan thanh cong',
-                    "data": new_user
-                })
-
-            except Exception as e:
-                print(e)
-                return jsonify({
-                    "message": 'Some error occurred. Please try again.',
-                })
-
-        else:
-            return jsonify({
-                "message": 'User already exists. Please Log in.'
-            })
-
-    def post(self):
-
-        args = parser.parse_args()
         user_id = args['user_id']
         movie_id = args['movie_id']
         rating = args['rating']
@@ -180,3 +139,47 @@ class LoginResource(Resource):
             return {
                 "message": 'Tai khoan khong chinh xac'
             }
+
+
+class RegisterResource(Resource):
+
+    def post(self):
+
+        args = parser.parse_args()
+        username = args['username']
+        password = args['password']
+
+        if not username:
+            return jsonify({
+                'message': 'username bat buoc nhap'
+            })
+
+        if not password:
+            return jsonify({
+                'message': 'password bat buoc nhap'
+            })
+
+        user = Users.objects(username=username).first()
+
+        if not user:
+            try:
+                user_id = Users.objects().count() + 1
+                new_user = Users(user_id=user_id, username=username,
+                                 password=password)
+                new_user.save()
+
+                return jsonify({
+                    "message": 'Tao tai khoan thanh cong',
+                    "data": new_user
+                })
+
+            except Exception as e:
+                print(e)
+                return jsonify({
+                    "message": 'Some error occurred. Please try again.',
+                })
+
+        else:
+            return jsonify({
+                "message": 'User already exists. Please Log in.'
+            })
